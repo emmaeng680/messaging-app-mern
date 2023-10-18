f0bfe6173576b4da87fd9e12de59fa38feb34917
Emmanuel Oppong <oppongemma73@gmail.com>
2023-10-18 17:23:15 +0400

    messaging-app-mern

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBeAkbRNwVTVBkLA631pZXrkquKXwO9RXc",
+    authDomain: "messaging-app-mern-fd82d.firebaseapp.com",
+    projectId: "messaging-app-mern-fd82d",
+    storageBucket: "messaging-app-mern-fd82d.appspot.com",
+    messagingSenderId: "24403887828",
+    appId: "1:24403887828:web:48eeb0a4742ef33ad39d41"
+  };
